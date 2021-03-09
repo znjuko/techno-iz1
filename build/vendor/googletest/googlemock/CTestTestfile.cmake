0a072903246fac36d5b21b4e9c5886cc0e17c870
Chernyh.Nikita2 <Chernyh.Nikita2@wildberries.ru>
2021-03-10 00:16:05 +0300

    added uploading arts of cvrg report

--- build/vendor/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chernyh.nikita2/techno/c_p_p/techno-iz1/vendor/googletest/googlemock
-# Build directory: /Users/chernyh.nikita2/techno/c_p_p/techno-iz1/build/vendor/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/vendor/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chernyh.nikita2/techno/c_p_p/techno-iz1/vendor/googletest/googletest
-# Build directory: /Users/chernyh.nikita2/techno/c_p_p/techno-iz1/build/vendor/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
